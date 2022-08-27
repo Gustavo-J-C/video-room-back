8fc5dcb062186f3fd1a755b178c429dc0ea05c11
Gustavo-J-C <62143585+Gustavo-J-C@users.noreply.github.com>
2022-08-27 11:35:47 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const router = express.Router();
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const {Server} = require('socket.io');
+
+const PORT = process.env.PORT || 80
+const server =  http.createServer(app);
+app.use(cors());
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+});
+
+let rooms = [];
+
+io.on("connection", (socket) => {
+
+    socket.emit("request_rooms", rooms);
+    console.log(`User Connected: ${socket.id}`);
+
+    socket.on("set_video", (data) => {
+        console.log("set video: ",data)
+        socket.to(data.room).emit("recieve_video", data.newLink)
+    })
+
+    socket.on("join_room", (data) => {
+        socket.join(data)
+        console.log(`User with ID: ${socket.id} joinend room: ${data}`);
+    })
+
+    socket.on("create_room", (data) => {
+        rooms.push(data);
+        socket.emit("request_rooms", rooms) 
+    })
+
+    socket.on("send_message", (data) => {
+        console.log(data);
+        socket.to(data.room).emit("recieve_message", data)
+    })
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnects", socket.id)
+    })
+})
+
+
+server.listen(PORT, () => {
+    console.log('server runing')
+})
